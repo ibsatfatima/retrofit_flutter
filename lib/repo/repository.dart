588af3a67f0b544ced24838a1efc832cd5026355
Ibsat <mlengineer456@gmail.com>
2023-02-16 21:26:39 +0500

    first commit

---
+++ lib/repo/repository.dart
@@ -0,0 +1,14 @@
+// import 'package:dio/dio.dart';
+//
+// import '../Api/api_client.dart';
+//
+// class AppRepository {
+//
+//   late ApiClient _apiRequest;
+//   late Dio dio;
+//
+//   AppRepos() {
+//     dio = Dio();
+//     _apiRequest = ApiClient(dio);
+//   }
+// }
